c6f2a95ffe522aff02251913fbaac4d2877e90f8
Mark Clifford <markmclifford@gmail.com>
2023-04-17 19:20:50 -0700

    blog move

--- pages/index.js
+++ pages/index.js
@@ -25,21 +25,7 @@ export default function Home({allPostsData}) {
       <p>Hello! My name is Mark Pascucci-Clifford. I'm an aspiring web developer, vibraphonist, and composer.</p>
         
       </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-          <li className={utilStyles.listItem} key={id}>
-          <Link href={`/posts/${id}`}>{title}</Link>
-          <br />
-          <small className={utilStyles.lightText}>
-            <Date dateString={date} />
-          </small>
-        </li>
-
-          ))}
-        </ul>
-      </section>
+     <Link href="/blog" className={utilStyles.headingLg} >Blog</Link>
     </Layout>
   );
 }
